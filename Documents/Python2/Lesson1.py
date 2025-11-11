725dacd647009df297495d03cb934e9eb8632de0
iliya <spoonfedfish0@gmail.com>
2025-11-11 18:53:04 +0100

    Add initial Python script

---
+++ Documents/Python2/Lesson1.py
@@ -0,0 +1,25 @@
+# print("hello", "meme", "hehe", sep = "\n", end ="!")
+# print("check")
+
+#print(f"hello world {x:.2f}")
+
+
+
+# x = 8
+# if x > 10:
+#     print("hello")
+#     print("more")
+# elif x < 10:
+#     print("less")
+# elif x==10:
+#     print("equal")
+
+
+# for i in range(1,6,1):
+#     print("* " * i)
+
+
+# for i in range(1,6,1):
+#     print("_" * (6 - i), "*" * i)
+
+
