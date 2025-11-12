bc6f9ebcf66e3fb1122b160469ba9b3888d3bda9
iliya <spoonfedfish0@gmail.com>
2025-11-12 13:24:41 +0100

    :3

--- Documents/Python2/Lesson1.py
+++ Documents/Python2/Lesson1.py
@@ -1,25 +1,60 @@
-# print("hello", "meme", "hehe", sep = "\n", end ="!")
-# print("check")
-
-#print(f"hello world {x:.2f}")
+# for i in range(6):
+#     print("*" * i)
 
 
 
-# x = 8
-# if x > 10:
-#     print("hello")
-#     print("more")
-# elif x < 10:
-#     print("less")
-# elif x==10:
-#     print("equal")
-
-
-# for i in range(1,6,1):
+# for i in range(6):
 #     print("* " * i)
 
 
-# for i in range(1,6,1):
-#     print("_" * (6 - i), "*" * i)
+
+# for i in range(5, 0, -1):
+#     print("* " * i)
+    
+    
+    
+# for i in range(1, 6 , 1):
+#     print(" " * (5-i), "*" * i)
+
+
+
+# for i in range(1, 7 , 1):
+#     print(" " * (6-i), "* " * i)
+
+
+
+
+# for i in range(10):
+#     print(" " * i + "*" * (2 * (10 - i) - 1))
+
+# for i in range(2, 10 + 1):
+#     print(" " * (10 - i) + "*" * (2 * i - 1))
+
+
+
+# for i in range(1, 11):
+#     print(" " * (10 - i) + "* " * i)
+
+# for i in range(9, 0, -1):
+#     print(" " * (10 - i) + "* " * i)
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+print( (a + 1) // 2 + (b + 1) // 2 + (c + 1) // 2)
+
+
+
+a = float(input())
+print( (a % 30) * 12)
+
+
+
+a = int(input())
+if (a % 4 == 0 and a % 100 != 0) or (a % 400 == 0):
+    print("LEAP")
+else:
+    print("COMMON")
 
 
---
+++ Documents/Python2/tree.py
@@ -0,0 +1,38 @@
+# for i in range(1,6,1):
+#     print("*" * i)
+
+# for i in range(1,6,1):
+#     print("* " * i)
+
+# for i in range(6,0,-1):
+#     print("* " * i)
+
+
+# for i in range(1,6,1):
+#     print(" " * (6 - i), "*" * i)
+
+
+# for i in range(1,6,1):
+#     print(" " * (6 - i), "* "  * i)
+
+# for i in range(0,6,1):
+#     print(" " * (6 - i), "*", "  "  * i, "*")
+
+
+# def avr(num1,num2):
+#     res = (num1+num2)/2
+#     return res
+
+
+# print(avr(10,8))
+
+
+# def tree(a):
+#     for i in range(1,a,1):
+#         print(" " * (a - i), "*" * i)
+#     return
+
+# tree(a)
+
+
+
